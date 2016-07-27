4303ebca1e4ab01135cffb1638e810a0346ca1bd
Andrew Jun <andrewjun@uber.biz>
2016-07-28 09:44:44 +1000

    remove encode - changing js code completely

--- js/index.js
+++ js/index.js
@@ -51,38 +51,23 @@ var app = {
     scan: function() {
         console.log('scanning');
         
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-        console.log('scanning1');
-
-        scanner.scan( function (result) { 
-
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
-
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
-            console.log(result);
-            /*
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-            }
-            */
-
-        }, function (error) { 
-            console.log("Scanning failed: ", error); 
-        },
-        {
-            "preferFrontCamera" : false, // iOS and Android
-            "showFlipCameraButton" : true, // iOS and Android
-            "prompt" : "Place a barcode inside the scan area", // supported on Android only
-            "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-            "orientation" : "portrait" // Android only (portrait|landscape), default unset so it rotates with the device
-        }
+        cordova.plugins.barcodeScanner.scan(
+          function (result) {
+              alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+          }, 
+          function (error) {
+              alert("Scanning failed: " + error);
+          },
+          {
+              "preferFrontCamera" : true, // iOS and Android
+              "showFlipCameraButton" : true, // iOS and Android
+              "prompt" : "Place a barcode inside the scan area", // supported on Android only
+              "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+              "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
+          }
        );
     },
 
