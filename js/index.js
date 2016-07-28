8d981cccfe24ca1d19c991f6dc4afb8ac4ea6e91
Andrew Jun <andrewjun@uber.biz>
2016-07-28 10:30:56 +1000

    portrait test

--- js/index.js
+++ js/index.js
@@ -66,7 +66,7 @@ var app = {
               "showFlipCameraButton" : true, // iOS and Android
               "prompt" : "Place a barcode inside the scan area", // supported on Android only
               "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-              "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
+              "orientation" : "portrait" // Android only (portrait|landscape), default unset so it rotates with the device
           }
        );
     },
