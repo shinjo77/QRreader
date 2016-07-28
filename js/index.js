5473ab5822a4c418476ff3af057cf203551cb893
Andrew Jun <andrewjun@uber.biz>
2016-07-28 11:36:08 +1000

    trying once more portrait

--- js/index.js
+++ js/index.js
@@ -52,23 +52,23 @@ var app = {
         console.log('scanning');
         
         cordova.plugins.barcodeScanner.scan(
-          function (result) {
-              alert("We got a barcode\n" +
-                    "Result: " + result.text + "\n" +
-                    "Format: " + result.format + "\n" +
-                    "Cancelled: " + result.cancelled);
-          }, 
-          function (error) {
-              alert("Scanning failed: " + error);
-          },
-          {
-              "preferFrontCamera" : true, // iOS and Android
-              "showFlipCameraButton" : true, // iOS and Android
-              "prompt" : "Place a barcode inside the scan area", // supported on Android only
-              "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-              "orientation" : "portrait" // Android only (portrait|landscape), default unset so it rotates with the device
-          }
-       );
+              function (result) {
+                  alert("We got a barcode not \n" +
+                        "Result: " + result.text + "\n" +
+                        "Format: " + result.format + "\n" +
+                        "Cancelled: " + result.cancelled);
+              }, 
+              function (error) {
+                  alert("Scanning failed: " + error);
+              },
+              {
+                  "preferFrontCamera" : true, // iOS and Android
+                  "showFlipCameraButton" : true, // iOS and Android
+                  "prompt" : "Place a barcode inside the scan area", // supported on Android only
+                  "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+                  "orientation" : "portrait" // Android only (portrait|landscape), default unset so it rotates with the device
+              }
+           );
     },
 
 
